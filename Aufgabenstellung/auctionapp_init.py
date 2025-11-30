43f0af83abd934c945f840af1b0a36951e8f7a72
mjohn2005 <mark.john@smail.th-koeln.de>
2025-11-30 20:06:13 +0100

    Update auctionapp_init.py
     AVL-Baum Auto-Vervollständigung

--- Aufgabenstellung/auctionapp_init.py
+++ Aufgabenstellung/auctionapp_init.py
@@ -520,8 +520,8 @@ class AuctionAppInit:
     def show_suggestions(self, event):
         search_text = self.search_entry.get()
         if search_text:
-            suggestions = self.trie.search(search_text)
-            # avl_suggestions = self.avl_tree.find_most_likely_words(search_text)
+            # AVL-Baum Auto-Vervollständigung
+            avl_suggestions = self.avl_tree.find_most_likely_words(Search_text)
             self.show_tooltip(suggestions)
         else:
             self.hide_tooltip()
@@ -730,4 +730,5 @@ class AuctionAppInit:
     @staticmethod
     def simple_input(title, prompt):
         return simpledialog.askstring(title, prompt)
+
 
